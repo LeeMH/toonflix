7d2566d7d9540023b531fd32be470c737679a68e
ifthenelse <lee.munhaeng@gmail.com>
2023-11-27 21:15:57 +0900

    lazy loading 되도록 ListView.Builder 사용

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -29,10 +29,13 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView(
-              children: [
-                for (var webtoon in snapshot.data!) Text(webtoon.title)
-              ],
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                WebtoonModel webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
             );
           } else {
             return const Center(
